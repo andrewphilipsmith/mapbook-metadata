b91c4a2ee753ba473918e6d8a2e1d02fda307260
Andy Smith <asmith@mapaction.org>
2015-05-03 13:28:55 +0545

    writing csv for HR.info

--- mapbook_export.py
+++ mapbook_export.py
@@ -18,9 +18,11 @@ def run_export(args):
 
     mxd_arg = args.MXD_file
     mxd = get_mxd(mxd_arg)
-    theme = args.theme
+    map_theme = args.map_type
+    hri_theme = args.hri_theme
     logging.info("args.MXD_file = {}".format(args.MXD_file))
-    logging.info("args.theme = {}".format(args.theme))
+    logging.info("args.map_type = {}".format(args.map_type))
+    logging.info("args.hri_theme = {}".format(args.hri_theme))
 
     logging.info("mxd name = {}".format(mxd.filePath))
     logging.info("mxd name = {}".format(mxd.filePath))
@@ -30,20 +32,27 @@ def run_export(args):
 
     map_subdir = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", "map_no")[0].text.strip()
     export_path = os.path.join(settings.output_dir, map_subdir)
+    if not os.path.exists(export_path):
+        os.mkdir(export_path)
     ma_web_csv_filename = os.path.join(export_path, "{}_ma_web.csv".format(rootfilename))
+    hri_csv_filename = os.path.join(export_path, "{}_hr_info.csv".format(rootfilename))
 
     ma_web_csv = open(ma_web_csv_filename, 'wb')
+    hri_csv = open(hri_csv_filename, 'wb')
     ma_web_csv_writer = csv.writer(ma_web_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    hri_csv_writer = csv.writer(hri_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     if mxd.isDDPEnabled:
-        export_data_driven_pages(mxd, rootfilename, theme, ma_web_csv_writer)
+        export_data_driven_pages(mxd, rootfilename, map_theme=map_theme, hri_theme=hri_theme, export_path=export_path,
+                                 ma_web_csv_writer=ma_web_csv_writer, hri_csv_writer=hri_csv_writer)
     else:
-        export_single_page(mxd, rootfilename, None, theme, ma_web_csv_writer)
+        export_single_page(mxd=mxd, rootfilename=rootfilename, location=None, map_theme=map_theme, hri_theme=hri_theme,
+                           export_path=export_path, ma_web_csv_writer=ma_web_csv_writer, hri_csv_writer=hri_csv_writer)
 
     ma_web_csv.close()
 
 
-def export_data_driven_pages(mxd, rootfilename, theme, ma_web_csv_writer):
+def export_data_driven_pages(mxd, rootfilename, map_theme, hri_theme, export_path, ma_web_csv_writer, hri_csv_writer):
 
     for pageNum in range(mxd.dataDrivenPages.pageCount):
 
@@ -52,11 +61,12 @@ def export_data_driven_pages(mxd, rootfilename, theme, ma_web_csv_writer):
         # filepath = sys.argv[2]
         page_name = mxd.dataDrivenPages.pageRow.getValue(mxd.dataDrivenPages.pageNameField.name)
         pagefilename = "{}_{}".format(rootfilename, page_name)
-        export_single_page(mxd, pagefilename, page_name, theme, ma_web_csv_writer)
+        export_single_page(mxd=mxd, rootfilename=rootfilename, location=None, map_theme=map_theme, hri_theme=hri_theme,
+                           export_path=export_path, ma_web_csv_writer=ma_web_csv_writer, hri_csv_writer=hri_csv_writer)
 
 
-def export_single_page(mxd, rootfilename, location, theme, ma_web_csv_writer):
-    export_path = settings.output_dir
+def export_single_page(mxd, rootfilename, location, map_theme, hri_theme, export_path, ma_web_csv_writer, hri_csv_writer):
+    first_page = True
 
     pdf_name = os.path.join(export_path, "{}.pdf".format(rootfilename))
     jpeg_name = os.path.join(export_path, "{}.jpeg".format(rootfilename))
@@ -91,18 +101,24 @@ def export_single_page(mxd, rootfilename, location, theme, ma_web_csv_writer):
                                jpeg_quality=settings.tmbnail_jpeg_quality)
     logging.info("Completed exporting Thumbnail")
 
-    metadata = export_mapaction_website_metadata(mxd, jpeg_name, pdf_name, location, theme)
-    for key, value in metadata.iteritems():
+    # Export MapAction website metadata
+    ma_metadata = export_mapaction_website_metadata(mxd=mxd, jpgfilename=jpeg_name, pdffilename=pdf_name,
+                                                    location=location, map_theme=map_theme)
+    hri_metadata = export_hr_info_metadata(location=location, map_theme=map_theme,
+                                           hri_theme=hri_theme, coord_hub=map_theme)
+    for key, value in ma_metadata.iteritems():
         logging.info("MA Web metadata key = {}\t value = {}".format(key, value))
 
-    ma_web_csv_writer.writerow(metadata.values())
+    for key, value in hri_metadata.iteritems():
+        logging.info("HR.info metadata key = {}\t value = {}".format(key, value))
 
-    # with open(ma_web_csv_filename, 'wb') as ma_web_csv:
-    #    ma_web_csv_writer = csv.writer(ma_web_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    #    ma_web_csv_writer.writerow(metadata.values())
-    # ma_web_csv.close()
+    if first_page:
+        first_page = False
+        ma_web_csv_writer.writerow(ma_metadata.keys())
+        hri_csv_writer.writerow(hri_metadata.keys())
 
-    # return pdf_filename, thumbnail_filename
+    ma_web_csv_writer.writerow(ma_metadata.values())
+    hri_csv_writer.writerow(hri_metadata.keys())
 
 
 def remove_newline_char(some_text):
@@ -127,7 +143,7 @@ def export_kiosk_metadata(mxd):
     """
     pass
 
-def export_mapaction_website_metadata(mxd, jpgfilename, pdffilename, location, theme):
+def export_mapaction_website_metadata(mxd, jpgfilename, pdffilename, location, map_theme):
     values = {}
     values["operationID"] = settings.maw_operational_id
     values["sourceorg"] = "MapAction"
@@ -174,33 +190,36 @@ def export_mapaction_website_metadata(mxd, jpgfilename, pdffilename, location, t
 
     values["jpgfilesize"] = 0
     values["pdffilesize"] = 0
-    values["theme"] = theme
+    values["theme"] = map_theme
 
     return values
 
 
-def export_hr_info_metadata(location, theme):
+def export_hr_info_metadata(location, map_theme, hri_theme, coord_hub):
+    """
+    DONE Title 	            The actual human readable title of the map
+    DONE Type 	            Reference, Thematic, etc. Note: Use the 'Map Type' controlled vocabulary in the site
+    File 	            The URL of file sitting in the Dropbox folder. Note: remove the www from the URL and put 'dl' instead
+    DONE Language 	        Language of the file. For English use 'en'
+    Cluster    "Inter Cluster"         Cluster(s) to which the map belongs to. For multiple cluster use ';' e.g. 'Protection;Emergency
+    DONE Publication Date 	Date when the map was published. Use the format, "dd-mmm-yyyy" i.e. "02-Jan-2011"
+    DONE Coordination Hub   "Kathmandu, Gorkha City names" 	Full name of the Coordination hub where the map could belong to. This should be already available in
+    DONE Location            Geographic location where the map belongs to. Please note that the location should already be
+    DONE Themes              Theme(s) where the person could belongs to. For multiple themes use ";" Example: "Age; Early Warning;"
+    DONE Emergencies 	    Emergency where the person could belong to. For multiple emergencies use ';' Example:
+    """
     values = {}
     values["title"] = remove_newline_char(arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT", "title")[0].text)
     values["Language"] = 'en'
     values["Publication Date"] = str(datetime.datetime.now().strftime('%Y-%b-%d'))
     values["Location"] = location
-    values["Themes"] = theme
+    values["Themes"] = hri_theme
     values["Emergencies"] = settings.hri_emergencies
+    values["Type"] = map_theme
+    values["Coordination"] = coord_hub
+    ## values["File"] =
 
-"""
-DONE Title 	            The actual human readable title of the map
-Type 	            Reference, Thematic, etc. Note: Use the 'Map Type' controlled vocabulary in the site
-File 	            The URL of file sitting in the Dropbox folder. Note: remove the www from the URL and put 'dl' instead
-DONE Language 	        Language of the file. For English use 'en'
-Cluster             Cluster(s) to which the map belongs to. For multiple cluster use ';' e.g. 'Protection;Emergency
-DONE Publication Date 	Date when the map was published. Use the format, "dd-mmm-yyyy" i.e. "02-Jan-2011"
-Coordination Hub 	Full name of the Coordination hub where the map could belong to. This should be already available in
-DONE Location            Geographic location where the map belongs to. Please note that the location should already be
-DONE Themes              Theme(s) where the person could belongs to. For multiple themes use ";" Example: "Age; Early Warning;"
-DONE Emergencies 	    Emergency where the person could belong to. For multiple emergencies use ';' Example:
-"""
-
+    return values
 
 def get_mxd(mxdfile):
     if mxdfile is None:
@@ -215,7 +234,10 @@ if __name__ == '__main__':
                     'metadata fields. Handles Data Driven Pages by exporting a individual file per page'
     )
     parser.add_argument('MXD_file')  # positional, rather than option.
-    parser.add_argument('-t', '--theme', default='Reference')
+    parser.add_argument('-t', '--hri_theme', default='Disaster Management')
+    parser.add_argument('-m', '--map_type', default='Reference Maps')
+    parser.add_argument('-c', '--cluster', default='Inter-Cluster Coordination')
+    parser.add_argument('-b', '--hub', default='Kathmandu')
     args = parser.parse_args()
 
     run_export(args)
